f02eafb83e60c110bc9a3459ca0c5c0f17616be0
mkizka <g.mkizka@gmail.com>
2021-09-27 12:33:21 +0900

    更新

--- engine/src/components/Senario.tsx
+++ engine/src/components/Senario.tsx
@@ -1,38 +1,13 @@
-import React, { useEffect } from "react";
-import { useMessage } from "../hooks/useMessage";
+import React from "react";
 
 export type SenarioProps = {
   children: React.ReactNode;
 };
 
-type SenarioItem = {
-  type: string;
-  text: string;
-  props?: object;
-};
-
-function getSenario(children: React.ReactNode) {
-  const texts = React.Children.toArray(children);
-  return texts.map((textElement) => {
-    if (!React.isValidElement(textElement)) return null;
-    const textChildren = React.Children.toArray(textElement.props.children);
-    return textChildren.reduce<SenarioItem[]>((result, textChild) => {
-      if (React.isValidElement(textChild)) {
-        const { children: text, ...props } = textChild.props;
-        // @ts-ignore
-        result.push({ type: textChild.type.name, text, props });
-      } else {
-        result.push({ type: "Plain", text: `${textChild}` });
-      }
-      return result;
-    }, []);
-  });
-}
-
+/*
+  使い方未定
+  背景など画面全体へのパラメータの初期値を決める？など
+*/
 export function Senario({ children }: SenarioProps) {
-  const message = useMessage();
-  useEffect(() => {
-    //message.setMessages(getSenario(children));
-  }, [children]);
   return <>{children}</>;
 }
